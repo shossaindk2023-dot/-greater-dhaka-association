7562a73655f94d09f153e3d83f66d65524b250ff
shossaindk2023-dot <shossaindk2023@gmail.com>
2026-09-16 18:02:23 +0200

    Add Google Search Console verification file endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,8 @@
 from app import app
+
+# Google Search Console HTML-file verification.
+@app.route('/google827a650554bab237.html')
+def google_site_verification():
+    return 'google-site-verification: google827a650554bab237.html', 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
 application = app
